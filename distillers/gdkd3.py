39c25b157c2916542caa62240239d7fd0723d2df
zaber <20830726+ZaberKo@users.noreply.github.com>
2025-06-12 10:59:03 +0800

    seperate gdkd3

--- distillers/gdkd3.py
+++ distillers/gdkd3.py
@@ -12,7 +12,7 @@ from .utils import kl_div
 
 
 @KD_REGISTRY.register()
-class GDKD(RCNNKD):
+class GDKD3(RCNNKD):
     @configurable
     def __init__(
         self,
@@ -68,16 +68,14 @@ class GDKD(RCNNKD):
             self.teacher.roi_heads, t_features, sampled_proposals
         )
 
-        losses["loss_gdkd"], info_dict = rcnn_gdkd_loss(
+        losses["loss_gdkd3"], info_dict = rcnn_gdkd3_loss(
             s_predictions,
             t_predictions,
             [x.gt_classes for x in sampled_proposals],
-            k=self.kd_args.GDKD.TOPK,
-            w0=self.kd_args.GDKD.W0,
-            w1=self.kd_args.GDKD.W1,
-            w2=self.kd_args.GDKD.W2,
-            temperature=self.kd_args.GDKD.T,
-            distill_type=self.kd_args.GDKD.DISTILL_TYPE,
+            w0=self.kd_args.GDKD3.W0,
+            w1=self.kd_args.GDKD3.W1,
+            temperature=self.kd_args.GDKD3.T,
+            distill_type=self.kd_args.GDKD3.DISTILL_TYPE,
         )
 
         self.record_info(info_dict)
@@ -92,8 +90,8 @@ class GDKD(RCNNKD):
         return losses
 
 
-def rcnn_gdkd_loss(
-    s_predictions, t_predictions, gt_classes, k, w0, w1, w2, temperature, distill_type
+def rcnn_gdkd3_loss(
+    s_predictions, t_predictions, gt_classes, w0, w1, temperature, distill_type
 ):
     s_logits, s_bbox_offsets = s_predictions
     t_logits, t_bbox_offsets = t_predictions
@@ -113,9 +111,7 @@ def rcnn_gdkd_loss(
         t_logits = t_logits[mask]
         empty_fg_flag = torch.count_nonzero(mask) == 0
 
-    loss_gdkd, loss_info_dict = gdkd_loss(
-        s_logits, t_logits, k, w0, w1, w2, temperature
-    )
+    loss_gdkd, loss_info_dict = gdkd3_loss(s_logits, t_logits, w0, w1, temperature)
 
     if empty_fg_flag:
         loss_gdkd = torch.zeros_like(loss_gdkd)
@@ -126,38 +122,43 @@ def rcnn_gdkd_loss(
     return loss_gdkd, info_dict
 
 
-def get_masks(logits, k=5):
+def get_masks3(logits):
     largest_flag = True
 
-    ranks = torch.topk(logits, k, dim=-1, largest=largest_flag, sorted=False).indices
+    ranks = torch.topk(logits, 2, dim=-1, largest=largest_flag, sorted=True).indices
 
-    # topk mask
-    mask_u1 = torch.zeros_like(logits, dtype=torch.bool).scatter_(1, ranks, 1)
+    # top1 mask (usually target)
+    mask_u1 = torch.zeros_like(logits, dtype=torch.bool).scatter_(1, ranks[:, 0:1], 1)
+    # top2 mask
+    mask_u2 = torch.zeros_like(logits, dtype=torch.bool).scatter_(1, ranks[:, 1:2], 1)
+
     # other mask
-    mask_u2 = torch.logical_not(mask_u1)
 
-    return mask_u1, mask_u2
+    not_mask_u3 = torch.zeros_like(logits, dtype=torch.bool).scatter_(1, ranks, 1)
+
+    mask_u3 = torch.logical_not(not_mask_u3)
+
+    return mask_u1, mask_u2, mask_u3, not_mask_u3
 
 
-def cat_mask(t, mask1, mask2):
+def cat_mask3(t, mask1, mask2, mask3):
     t1 = (t * mask1).sum(dim=1, keepdims=True)
     t2 = (t * mask2).sum(dim=1, keepdims=True)
-    rt = torch.cat([t1, t2], dim=1)  # [B, 2]
+    t3 = (t * mask3).sum(dim=1, keepdims=True)
+    rt = torch.cat([t1, t2, t3], dim=1)  # [B, 3]
     return rt
 
 
-def gdkd_loss(
+def gdkd3_loss(
     logits_student,
     logits_teacher,
-    k,
     w0,
     w1,
-    w2,
     temperature,
     mask_magnitude=1000,
     kl_type="forward",
 ):
-    mask_u1, mask_u2 = get_masks(logits_teacher, k)
+    mask_u1, mask_u2, mask_u3, not_mask_u3 = get_masks3(logits_teacher)
 
     soft_logits_student = logits_student / temperature
     soft_logits_teacher = logits_teacher / temperature
@@ -167,37 +168,26 @@ def gdkd_loss(
 
     # Notation: high_loss: level 0 loss; low_loss: level 1 loss
     # accumulated term
-    p0_student = cat_mask(p_student, mask_u1, mask_u2)
-    p0_teacher = cat_mask(p_teacher, mask_u1, mask_u2)
+    p0_student = cat_mask3(p_student, mask_u1, mask_u2, mask_u3)
+    p0_teacher = cat_mask3(p_teacher, mask_u1, mask_u2, mask_u3)
 
     log_p0_student = torch.log(p0_student)
     high_loss = F.kl_div(log_p0_student, p0_teacher, reduction="batchmean") * (
         temperature**2
     )
-
-    # topk loss
+    # other classes loss
     log_p1_student = F.log_softmax(
-        soft_logits_student - mask_magnitude * mask_u2, dim=1
+        soft_logits_student - mask_magnitude * not_mask_u3, dim=1
     )
     log_p1_teacher = F.log_softmax(
-        soft_logits_teacher - mask_magnitude * mask_u2, dim=1
+        soft_logits_teacher - mask_magnitude * not_mask_u3, dim=1
     )
-    low_top_loss = kl_div(log_p1_student, log_p1_teacher, temperature, kl_type)
+    low_other_loss = kl_div(log_p1_student, log_p1_teacher, temperature, kl_type)
 
-    # other classes loss
-    log_p2_student = F.log_softmax(
-        soft_logits_student - mask_magnitude * mask_u1, dim=1
-    )
-    log_p2_teacher = F.log_softmax(
-        soft_logits_teacher - mask_magnitude * mask_u1, dim=1
-    )
-    low_other_loss = kl_div(log_p2_student, log_p2_teacher, temperature, kl_type)
-
-    gdkd_loss = w0 * high_loss + w1 * low_top_loss + w2 * low_other_loss
+    gdkd_loss = w0 * high_loss + w1 * low_other_loss
 
     info = dict(
         loss_high=high_loss.detach(),
-        loss_low_top=low_top_loss.detach(),
         loss_low_other=low_other_loss.detach(),
     )
 
